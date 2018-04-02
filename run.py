327e4910fa338bb2d85fe3090a5972c05babde75
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-03 00:16:01 +0300

    add save_credentials function that takes in credentials object and calls the save_credentials method to save the credentials

--- run.py
+++ run.py
@@ -2,9 +2,18 @@ from credentials import Credentials
 from user import User
 import random
 
+
 def create_credentials(website, username, password):
-	'''
-    Function to create a new credentials
     '''
+Function to create a new credentials
+'''
     new_credentials = Credentials(website, username, password)
     return new_credentials
+
+
+def save_credentials(credentials):
+    '''
+    function to save credentials
+    '''
+    credentials.save_credentials()
+
