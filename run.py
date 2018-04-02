46cc3e12fe3be4e06d04eaf94695dceae8aa1a96
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-03 00:59:44 +0300

    add init function and welcome message

--- run.py
+++ run.py
@@ -3,6 +3,21 @@ from user import User
 import random
 
 
+def create_user(first_name, last_name, email, login_password):
+    '''
+Function to create a new user
+'''
+    new_user = User(first_name, last_name, email, login_password)
+    return new_user
+
+
+def save_user(user):
+    '''
+Function to save user
+'''
+    user.save_user
+
+
 def create_credentials(website, username, password):
     '''
 Function to create a new credentials
@@ -34,6 +49,22 @@ def find_credentials(website):
 
 def find_existing_credentials(website):
     '''
-Function that check if a credential exists with that website and return a Boolean
-'''
+        Function that check if a credential exists with that website and return a Boolean
+        '''
     return Credentials.credentials_exist(website)
+
+
+def display_credentials():
+    '''
+        Function that returns all the saved credentials
+        '''
+    return Credentials.display_credentials()
+
+
+def main():
+    print("Hello and welcome to your Passwords Vault. An app that help you generate and/or store passwords for your numerous accounts")
+    print('\n')
+
+
+if __name__ == '__main__':
+    main()
