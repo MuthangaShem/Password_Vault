5e0515c7e7649a4fcedfa509259c8a4fb40f354c
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-04-02 20:03:41 +0300

    create a save_credentials method that is called on a contact object and appends it to our contact-list list.

--- credentials.py
+++ credentials.py
@@ -17,8 +17,3 @@ class Credentials:
         self.username = username
         self.password = password
 
-    def save_credentials(self):
-        '''
-        save_credentials method saves credentials objects into credentials_list
-        '''
-        Credentials.credentials_list.append(self)
